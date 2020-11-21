771087bbc3df7d8698f74dd105682095af4efe5a
Wes <wesweaver1@gmail.com>
2020-11-20 23:29:59 -0500

    add the analysis for the tweets

---
+++ analysis/positive_tests_poverty_shootings.R
@@ -0,0 +1,161 @@
+library(tidyverse)
+library(ggrepel)
+library(tidycensus)
+library(sf)
+
+# get covid test data from opendataphilly
+tests <- read.csv(
+  "https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+covid_cases_by_date&filename=covid_cases_by_date&format=csv&skipfields=cartodb_id",
+  stringsAsFactors = F
+) %>%
+  mutate(collection_date = as.Date(collection_date)) %>%
+  arrange(collection_date)
+
+# pull psotive cases and calculate 7 day lagged moving average
+positive_cases <- tests %>%
+  filter(test_result == "positive") %>%
+  mutate(positive_7day_average = zoo::rollmean(
+    count, k=7, fill = NA, align = "right"
+  ))
+
+# get highest number of positive tests as single row data frame
+high_point <- positive_cases %>%
+  filter(count == max(count)) %>%
+  mutate(text_label = glue::glue("Single Day High\n{format(collection_date, '%b %d, %Y')}\nCount: {count}"))
+
+# plot daily tests and moving average with highest point indicated
+positive_cases %>%
+  ggplot()+
+  geom_col(aes(x = collection_date, y = count, fill = "Positive Tests"), color = "#349AE9")+
+  geom_line(data = positive_cases, aes(x = collection_date, y = positive_7day_average, color = "7 Day Average"), size = 2)+#, color = "#1e1e1f")+
+  geom_text_repel(data = high_point,
+                  aes(x = collection_date,
+                      y = count,
+                      label = glue::glue("Single Day High\n{format(collection_date, '%b %d, %Y')}\nCount: {count}")
+                  ),
+                  fontface = "bold",
+                  #nudge_x = -2.5,
+                  nudge_y = 2,
+                  box.padding = 2.5,
+                  size = 3.5,
+                  segment.size = 1.1
+  )+
+  scale_x_date(date_breaks = "1 month", date_labels = "%b")+
+  scale_color_manual(name = "", values = c("#1e1e1f"))+
+  scale_fill_manual(name = "", values = c("#349AE9"))+
+  theme_minimal()+
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  )+
+  labs(
+    title = "Daily Positive COVID-19 Tests in Philadelphia",
+    x = "Collection Date",
+    y = "Count",
+    caption = "Source: OpenDataPhilly COVID Tests and Cases (www.opendataphilly.org/dataset/covid-cases)\nGraphic by @WesWeaver | wesmapping.com"
+  )
+
+
+# pull zip codes and tests by zip codes from opendataphilly. rename covid status with actual words.
+zip_codes <- st_read("http://data.phl.opendata.arcgis.com/datasets/b54ec5210cee41c3a884c9086f7af1be_0.geojson", quiet = T)
+tests_by_zip <- read.csv('https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+covid_cases_by_zip&filename=covid_cases_by_zip&format=csv&skipfields=cartodb_id',
+                         stringsAsFactors = F) %>%
+  mutate(
+    covid_status = case_when(
+      covid_status == "POS" ~ "Positive",
+      covid_status == "NEG" ~ "Negative"
+      ),
+    zip_code = as.character(zip_code)
+  ) %>%
+  dplyr::select(-the_geom, -the_geom_webmercator)
+
+census_data <- get_acs(
+  geography = "zcta",
+  variables = c(medincome = "B19013_001", total_pop = "B01003_001", total_pov = "B17001_002"),
+  year = 2018
+) %>%
+  filter(GEOID %in% zip_codes$CODE)
+
+census_data <- census_data %>%
+  pivot_wider(id_cols = c(GEOID), names_from = variable, values_from = estimate) %>%
+  mutate(pov_rate= round(total_pov / total_pop, 2))
+
+census_data %>%
+  left_join(
+    tests_by_zip %>%
+      filter(covid_status == "Positive"),
+    by = c("GEOID" = "zip_code")
+  ) %>%
+  mutate(positive_per_thousand = round(count/total_pop * 1000, 2)) %>%
+  ggplot(aes(x = pov_rate, y = positive_per_thousand, size = count, label = GEOID))+
+  geom_point(aes(x = pov_rate, y = positive_per_thousand), color = "#349AE9", alpha = .7)+
+  geom_smooth(method="lm", se = FALSE, color = "#349AE9")+
+  scale_size_continuous(breaks = c(500, 1000, 1500, 2000), name = "Total Positive Tests")+
+  geom_text_repel(
+    size = 3.5,
+    point.padding = 0.1,
+    min.segment.length = 0,
+    box.padding = 0.4
+  )+
+  guides(size = guide_legend(override.aes = list(linetype = 0)))+
+  scale_x_continuous(label = scales::percent)+
+  theme_minimal()+
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  )+
+  labs(
+    title = "Philadelphia Positive COVID-19 Tests and Poverty by ZIP Code",
+    x = "Poverty Rate",
+    y = "Positive Tests Per Thousand Residents",
+    caption = "Source: OpenDataPhilly COVID Tests and Cases (www.opendataphilly.org/dataset/covid-cases)\nand the U.S. Census ACS Estimates for 2018 (www.census.gov/programs-surveys/acs)\nGraphic by @WesWeaver | wesmapping.com"
+  )
+
+# get daily shooting data by zip code
+shootings <- st_read('https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+shootings&filename=shootings&format=geojson&skipfields=cartodb_id', quiet=T)
+
+# filter to only shootings that occurred the same day or after the first test results and spatial join zip codes
+shootings_in_period <- shootings %>%
+  filter(date_ >= min(positive_cases$collection_date),
+         officer_involved == "N") %>%
+  st_join(zip_codes %>% st_transform(crs = st_crs(shootings)), join = st_intersects)
+
+census_data %>%
+  left_join(
+    shootings_in_period %>%
+      st_drop_geometry() %>%
+      group_by(CODE) %>%
+      summarize(shootings = n()),
+    by = c("GEOID" = "CODE")
+  ) %>%
+  replace_na(list(shootings = 0)) %>%
+  left_join(
+    tests_by_zip %>%
+      filter(covid_status == "Positive"),
+    by = c("GEOID" = "zip_code")
+  ) %>%
+  mutate(positive_per_thousand = round(count/total_pop * 1000, 2)) %>%
+  mutate(shootings_per_thousand = round(shootings/total_pop * 1000, 2)) %>%
+  ggplot(aes(x = shootings_per_thousand, y = positive_per_thousand, size = count, label = GEOID))+
+  geom_point(aes(x = shootings_per_thousand, y = positive_per_thousand), color = "#349AE9", alpha = .7)+
+  geom_smooth(method="lm", se = FALSE, color = "#349AE9")+
+  scale_size_continuous(breaks = c(500, 1000, 1500, 2000), name = "Total Positive Tests")+
+  geom_text_repel(
+    size = 3.5,
+    point.padding = 0.1,
+    min.segment.length = 0,
+    box.padding = 0.4
+  )+
+  guides(size = guide_legend(override.aes = list(linetype = 0)))+
+  theme_minimal()+
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  )+
+  labs(
+    title = "Philadelphia Positive COVID-19 Tests and Shootings by ZIP Code",
+    subtitle = glue::glue("{format(min(positive_cases$collection_date), '%b %d %Y')} through {format(max(positive_cases$collection_date), '%b %d %Y')}"),
+    x = "Shootings Per Thousand Residents",
+    y = "Positive Tests Per Thousand Residents",
+    caption = "Source: OpenDataPhilly COVID Tests and Cases (www.opendataphilly.org/dataset/covid-cases)\nand Shooting Victims (www.opendataphilly.org/dataset/shooting-victims)\nGraphic by @WesWeaver | wesmapping.com"
+  )
